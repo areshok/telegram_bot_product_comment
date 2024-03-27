00300d3bb21f9802e62e6ba4a424deded76dc8bf
areshok <arsturlapov@yandex.ru>
2024-03-27 15:34:03 +0300

    the intermediate result of the site

--- telegram_bot/telegram_bot/urls.py
+++ telegram_bot/telegram_bot/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'1/', include('product.urls'))
+    path('', include('product.urls')),
+    #path('api/', include('api.urls')),
 ]
